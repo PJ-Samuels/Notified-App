c3b96c97b8ef68ce4bc99204ab6c76f8885ed6c4
Oliver Samuels <osamuels@bu.edu>
2023-06-16 12:14:45 -0400

    start artist search

--- client/src/components/artist_search.js
+++ client/src/components/artist_search.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { useEffect } from "react";
+import {useNavigate} from 'react-router-dom';
 
 
 export default function ArtistSearch() {
-    return(<div>Artist Search page</div>);
+
+    return(
+    <div>
+        <h1>Artist Search</h1>
+        <form>
+            <input type = "text" placeholder="Type Artist Name here"></input>
+            <input type = "submit" value = "Search"></input>
+        </form>
+        
+    </div>);
 }
