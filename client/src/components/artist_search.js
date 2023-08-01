eedbed1174d75977d00ff6b38894a3bb94a77c34
Oliver Samuels <osamuels@bu.edu>
2023-08-01 11:32:02 -0400

    version 3 notication preferences

--- client/src/components/artist_search.js
+++ client/src/components/artist_search.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import {useNavigate} from 'react-router-dom';
+// import {useNavigate} from 'react-router-dom';
 import './css/artist_search.css'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
