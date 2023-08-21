c9f8e587b2d14fe96b6ae759708ac05f427a4493
Oliver Samuels <osamuels@bu.edu>
2023-08-21 14:39:31 -0400

    minor changes

--- server/index.js
+++ server/index.js
@@ -61,7 +61,7 @@ app.use(express.json());
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '../client/build')))
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../client/build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err);
