99ead1989ab8fa898f65d99a15fe0d09f02a154f
Oliver Samuels <osamuels@bu.edu>
2023-08-21 16:31:42 -0400

    url testing

--- server/index.js
+++ server/index.js
@@ -209,7 +209,8 @@ app.get('/api/login', function(req, res) {
     state: state,
   })
   spotifyAuthUrl = 'https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString();
-  res.send(spotifyAuthUrl);
+  console.log(spotifyAuthUrl);
+  // res.send(spotifyAuthUrl);
 });
   
 app.get('/api/callback', function(req, res) {
