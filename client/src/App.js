e57677eb2be244cc95ba8c51643b8a42c9897096
Oliver Samuels <osamuels@bu.edu>
2023-08-21 12:38:09 -0400

    simplified version testing

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
 
   useEffect(() => {
       // fetch("http://localhost:5000/")
-      fetch("https://notified-webapp-0f26d6f34016.herokuapp.com/")
+      fetch("/")
         .then((res) => res.json())
         .then((data) => setData(data));
   }, []);
@@ -29,7 +29,7 @@ const App = () => {
   const handleClick = (event) => {
     event.preventDefault();
     // fetch("http://localhost:5000/",{
-    fetch("https://notified-webapp-0f26d6f34016.herokuapp.com/",{
+    fetch("/",{
       method: "POST",
       headers: {'Content-Type': "application/json"},
       body: JSON.stringify({email,password})
