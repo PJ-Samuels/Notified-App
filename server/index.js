ea1d97394569d831ee6f0d9620733995a0486bca
Oliver Samuels <osamuels@bu.edu>
2023-08-23 10:24:33 -0400

    branch switch

--- server/index.js
+++ server/index.js
@@ -120,7 +120,7 @@ app.get("/api/auth", (req, res) => {
   // const user_id = result.rows[0].id;
   // req.session.user_id = user_id;
   // res.header("Access-Control-Allow-Origin", "https://notified-webapp-0f26d6f34016.herokuapp.com");
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   // response.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
 
 
