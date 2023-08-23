785cad4bec98039ad4c9d09b37eaec52465ced01
Oliver Samuels <osamuels@bu.edu>
2023-08-23 12:50:13 -0400

    login url change

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -7,7 +7,7 @@ export default function Login() {
       sessionStorage.setItem('user_id', user_id);
 
       // fetch("/api/auth")
-      fetch(`http://localhost:5000/api/login?user_id=${user_id}`)
+      fetch(`https://notified-webapp-0f26d6f34016.herokuapp.com/api/login?user_id=${user_id}`)
       .then((res) => res.text())
       .then((spotifyAuthUrl) => {
         console.log("spotify auth",spotifyAuthUrl)
